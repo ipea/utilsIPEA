bd992c3d7272a1b0d231d599fd9b03baa2e2e85f
Gustavo Coelho <gutorc@hotmail.com>
2017-09-06 16:14:18 -0300

    Fixing NAMESPACE
    Signed-off-by: Gustavo Coelho <gutorc@hotmail.com>

---
+++ R/text_functions.R
@@ -0,0 +1,33 @@
+#' Remove de da e dos of names .
+#'
+#' \code{remove_preposicao_nomes} return names without de, da e dos.
+#'
+#'
+#' @param base A data table or data frame.
+#' @param column Caracter, the column with the names.
+#' @param ... more columns for apply the function
+#'
+#' @import data.table
+#' @importFrom stringr str_replace_all
+#' @return the base param with a new column.
+#'
+#' @export
+#'
+
+# retira "DE" "DA" e "DOS" dos nomes
+remove_preposicao_nomes <- function(base, column, ...){
+  other_columns <- unlist(eval(substitute(alist(...))))
+  subs <- " DA | DE | DOS | D. | DAS | A | B | C | D | E | F | G | H | I | J | K | L | M | N | O | P | Q | R | S | T | U | V | X | Z"
+  stopifnot(is.character(column))
+  stopifnot(is.character(other_columns))
+
+  if(!is.data.table(base)){
+    setDT(base)
+  }
+  new_columns <- sapply(other_columns, function(x) paste0(x,"_semD"))
+  base[,"nome_semD":=str_replace_all(get(column),subs, " ")]
+  mapply( function(x, y){ set(base, j = x, value = str_replace_all(base[[y]], subs, " ")) },
+          new_columns, other_columns)
+  return(base)
+
+}
