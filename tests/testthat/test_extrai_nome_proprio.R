073706f17a20d9f187db091ee5abb278a097fe04
igornoberto <igor.noberto@outlook.com>
2017-09-28 17:41:32 -0300

    # This is a combination of 4 commits. # This is the 1st commit message:
    Extrai nome proprio
    
    adapta nova base
    
    Adiciona funcoes de disctanciamento de strings e espaçamento
    
    corrige sobrenomes, traduz parametros e melhora retorno
    
    Corrige bugs e adiciona testes
    
    corrige sobrenomes, traduz parametros e melhora retorno
    
    # This is the commit message #2:
    
    melhora forma de exclusao de colunas
    
    # This is the commit message #3:
    
    Atualiza para nova base no github
    
    # This is the commit message #4:
    
    Atualiza

--- tests/testthat/test_extrai_nome_proprio.R
+++ tests/testthat/test_extrai_nome_proprio.R
@@ -1,7 +1,24 @@
 test_that("nomes proprios", {
-  nomes <- c("Carlos das Neves", "Pedro dos Anjos", "Maria Juvelina")
-  nomes_proprios <- c("CARLOS","PEDRO","MARIA")
+  nomes <- c("Carlos das Neves", "Pedro dos Anjos", "Ana Santos", "Edileusa maria Ferreira", "Maria Eduarda Almeida")
+  nomes_proprios <- c("CARLOS","PEDRO","ANA","EDILEUSA MARIA","MARIA EDUARDA")
   nomes_retorno <- extrai_NomeProprio(nomes)
-  expect_equal(c("nome", "NomeProprio"), names(nomes_retorno))
-  expect_equal(nomes_proprios, nomes_retorno$NomeProprio)
+  expect_equal(nomes_proprios, nomes_retorno)
+})
+
+
+test_that("nomes proprios com sexo", {
+  nomes <- c("Carlos das Neves", "Pedro dos Anjos", "Ana Santos", "Edileusa maria Ferreira", "Maria Eduarda Almeida")
+  nomes_proprios <- data.frame(NomeProprio = c("CARLOS","PEDRO","ANA","EDILEUSA MARIA","MARIA EDUARDA"), gender = c(2,2,1,1,1), stringsAsFactors = FALSE)
+  nomes_retorno <- extrai_NomeProprio(nomes,gender = TRUE)
+  expect_equal(c("NomeProprio","gender"), names(nomes_retorno))
+  expect_equal(nomes_proprios, nomes_retorno)
+})
+
+
+test_that("nomes proprios com sobrenome", {
+  nomes <- c("Carlos das Neves", "Pedro dos Anjos", "Ana Santos", "Edileusa maria Ferreira", "Maria Eduarda Almeida")
+  nomes_proprios <- data.frame(NomeProprio = c("CARLOS","PEDRO","ANA","EDILEUSA MARIA","MARIA EDUARDA"), surname = c("DAS NEVES", "DOS ANJOS", "SANTOS", "FERREIRA", "ALMEIDA"), stringsAsFactors = FALSE)
+  nomes_retorno <- extrai_NomeProprio(nomes,surname = TRUE)
+  expect_equal(c("NomeProprio","surname"), names(nomes_retorno))
+  expect_equal(nomes_proprios, nomes_retorno)
 })
