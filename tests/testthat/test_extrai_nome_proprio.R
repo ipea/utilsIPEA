6914c2296a486040ed51138927c817201e50af31
igornoberto <igor.noberto@outlook.com>
2017-10-06 14:45:43 -0300

    base para a função extrai_NomeProprio
    Signed-off-by: igornoberto <igor.noberto@outlook.com>

--- tests/testthat/test_extrai_nome_proprio.R
+++
@@ -1,7 +0,0 @@
-test_that("nomes proprios", {
-  nomes <- c("Carlos das Neves", "Pedro dos Anjos", "Maria Juvelina")
-  nomes_proprios <- c("CARLOS","PEDRO","MARIA")
-  nomes_retorno <- extrai_NomeProprio(nomes)
-  expect_equal(c("nome", "NomeProprio"), names(nomes_retorno))
-  expect_equal(nomes_proprios, nomes_retorno$NomeProprio)
-})
