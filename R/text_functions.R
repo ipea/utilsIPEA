0d28189f3a97421184472af731c3f5deb52181ac
Gustavo Rodrigues Coelho <B15599226@ipea.gov.br>
2017-10-10 12:57:05 -0300

    Generic function to base
    Signed-off-by: Gustavo Rodrigues Coelho <B15599226@ipea.gov.br>

--- R/text_functions.R
+++ R/text_functions.R
@@ -35,38 +35,6 @@ remove_preposicao_nomes <- function(base, ...){
 }
 
 
-#' Abrevia o nome do meio.
-#'
-#' \code{abrevia_nome_meio} return names .
-#'
-#'
-#' @param base A data table, data frame or character vector.
-#' @param ... columns for apply the function
-#'
-#' @import data.table
-#' @importFrom stringr str_replace_all
-#' @importFrom stringr str_extract
-#' @return the base param with a new column.
-#'
-#' @examples
-#'    base <- data.frame(nome = c("João Pereira Neves", "Pedro Aparecido Anjos", "Maria Joaquina Gracas"))
-#'    base <- remove_preposicao_nomes(base, "nome")
-#' @export
-#'
-abrevia_nome_meio <- function(base, ...){
-  if(is.character(base)){
-    return(abrevia_nomes_meio_coluna(base))
-  }
-  other_columns <- unlist(eval(substitute(alist(...))))
-  stopifnot(length(other_columns) > 0)
-  if(!is.data.table(base)){ setDT(base) }
-  new_columns <- sapply(other_columns, function(x) paste0(x, "_abrev"))
-  mapply( function(x, y){ set(base, j = x, value = abrevia_nomes_meio_coluna(base[[y]])) },
-          new_columns, other_columns)
-
-  return(base)
-
-}
 
 abrevia_nomes_meio_coluna<- function(nomes){
   novos_nomes <- sapply(nomes, USE.NAMES = F, function(nome){
@@ -85,3 +53,42 @@ abrevia_nomes_meio_coluna<- function(nomes){
 }
 
 
+funcao_generica <- function(base, ..., suffixo, FUN){
+  FUN <- match.fun(FUN)
+  if(is.character(base)){
+    return(FUN(base))
+  }
+  other_columns <- unlist(eval(substitute(alist(...))))
+  stopifnot(length(other_columns) > 0)
+  if(!is.data.table(base)){ setDT(base) }
+  new_columns <- sapply(other_columns, function(x) paste0(x, suffixo))
+  mapply( function(x, y){ set(base, j = x, value = FUN(base[[y]])) },
+          new_columns, other_columns)
+
+  return(base)
+
+}
+
+
+#' Abrevia o nome do meio.
+#'
+#' \code{abrevia_nome_meio} return names .
+#'
+#'
+#' @param base A data table, data frame or character vector.
+#' @param ... columns for apply the function
+#'
+#' @import data.table
+#' @importFrom stringr str_replace_all
+#' @importFrom stringr str_extract
+#' @return the base param with a new column.
+#'
+#' @examples
+#'    base <- data.frame(nome = c("Jo?o Pereira Neves", "Pedro Aparecido Anjos", "Maria Joaquina Gracas"))
+#'    base <- remove_preposicao_nomes(base, "nome")
+#' @export
+#'
+abrevia_nome_meio <- function(base, ..., suffixo = "_abrev"){
+  return(funcao_generica(base, ..., suffixo = suffixo, FUN = abrevia_nomes_meio_coluna))
+
+}
