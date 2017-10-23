b1227a74d997aa381f87c87bf8e87e9d76d1d9d7
unknown <B2688121@sstata2.ipea.gov.br>
2017-10-23 18:22:26 -0200

    Funcao que detecta erros de mun GALILEO
    Signed-off-by: unknown <B2688121@sstata2.ipea.gov.br>

---
+++ R/geocode.R
@@ -0,0 +1,37 @@
+#'
+#'
+#' \code{ident_erros_munic_galileo} Returns a new column called munmatch with true or false. This column identify where GALILEO failed
+#'
+#' @importFrom stringr str_replace_all
+#' @param base Data frame, data set with return from GALILEO
+#' @param mun character, the name of the municipio.
+#' @param match character, the colum MatchAdress from GALILEO.
+#' @param uf character, the name of the state.
+#'
+#' @return Returns a new column called munmatch with true or false.
+#'
+#' @examples
+#'   ident_erros_munic_galileo(base = rais2015, mun= rais2015$municipio,match= rais2015$MatchAdress , uf= rais2015$uf)
+#' @export
+#'
+ident_erros_munic_galileo <- function(base,mun,match,uf){
+  base<-as.data.table(base)
+  mun<-as.character(mun)
+  match<-as.character(match)
+  Encoding(mun)<-"latin1"
+  Encoding(match)<-"latin1"
+  base[,ufnovo:=uf %>% iconv(to="ASCII//TRANSLIT", from = "latin1") %>% toupper() %>% str_replace_all(fixed("'"), "") %>% str_replace_all("D.(S||)\\b", "") %>%
+         str_replace_all("[IY]","I") %>% str_replace_all("[ZS]","S") %>% str_replace_all("[JG]","G")]
+  base[,cepmatch:=str_detect(match,",\\s[0-9]{5}")]
+  base[,munstr:= mun %>% iconv(to="ASCII//TRANSLIT", from = "latin1") %>% toupper() %>% str_replace_all(fixed("'"), "") %>% str_replace_all(" D.(S||)\\b", "") %>%
+         str_replace_all("[IY]","I") %>% str_replace_all("[ZS]","S")%>% str_replace_all("[JG]","G")]
+  base[,matchstr:= match %>% iconv(to="ASCII//TRANSLIT", from = "latin1") %>% toupper() %>% str_replace_all(fixed("'"), "") %>% str_replace_all(" D.(S||)\\b", "") %>%
+         str_replace_all("[IY]","I") %>% str_replace_all("[ZS]","S")%>% str_replace_all("[JG]","G")]
+  base[,munmatch:= mapply(
+    function(x,y,c,d){ifelse(str_count(y, ",")<=1,str_detect(y,paste(x,",",sep='')) & str_detect(y,toupper(d))
+                             ,ifelse(str_count(y, ",")==2 && c==TRUE,str_detect(y,paste(x,",",sep='')) & str_detect(y,toupper(d)),str_detect(y,paste(", ",x,",",sep='')) & str_detect(y,toupper(d))))}
+    ,munstr,matchstr,cepmatch,ufnovo)]
+
+  base <- base[,!c("munstr", "matchstr", "ufnovo", "cepmatch"),with=F]
+  return(base)
+}
