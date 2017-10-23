062987f578261301063877d357a8eee4afb9e81c
igornoberto <igor.noberto@outlook.com>
2017-10-23 18:55:19 -0200

    Melhora testes

--- tests/testthat/test_extrai_nome_proprio.R
+++ tests/testthat/test_extrai_nome_proprio.R
@@ -7,8 +7,8 @@ test_that("nomes proprios", {
 
 
 test_that("nomes proprios com sexo", {
-  nomes <- c("Carlos das Neves", "Pedro dos Anjos", "Ana Santos", "Edileusa maria Ferreira", "Maria Eduarda Almeida")
-  nomes_proprios <- data.frame(NomeProprio = c("CARLOS","PEDRO","ANA","EDILEUSA MARIA","MARIA EDUARDA"), gender = c(2,2,1,1,1), stringsAsFactors = FALSE)
+  nomes <- c("Edileusamaria Ferreira", "Carlos das Neves", "Pedro dos Anjos", "Ana Santos", "Maria Eduarda Almeida")
+  nomes_proprios <- data.frame(NomeProprio = c("EDILEUSA MARIA","CARLOS","PEDRO","ANA","MARIA EDUARDA"), gender = c(1,2,2,1,1), stringsAsFactors = FALSE)
   nomes_retorno <- extrai_NomeProprio(nomes,gender = TRUE)
   expect_equal(c("NomeProprio","gender"), names(nomes_retorno))
   expect_equal(nomes_proprios, nomes_retorno)
@@ -17,8 +17,8 @@ test_that("nomes proprios com sexo", {
 
 test_that("nomes proprios com sobrenome", {
   nomes <- c("Carlos das Neves", "Pedro dos Anjos", "Ana Santos", "Edileusa maria Ferreira", "Maria Eduarda Almeida")
-  nomes_proprios <- data.frame(NomeProprio = c("CARLOS","PEDRO","ANA","EDILEUSA MARIA","MARIA EDUARDA"), surname = c("DAS NEVES", "DOS ANJOS", "SANTOS", "FERREIRA", "ALMEIDA"), stringsAsFactors = FALSE)
-  nomes_retorno <- extrai_NomeProprio(nomes,surname = TRUE)
+  nomes_proprios <- data.frame(NomeProprio = c("CARLOS","PEDRO","ANA","EDILEUSAMARIA","MARIAEDUARDA"), surname = c("DASNEVES", "DOSANJOS", "SANTOS", "FERREIRA", "ALMEIDA"), stringsAsFactors = FALSE)
+  nomes_retorno <- extrai_NomeProprio(nomes,surname = FALSE, spaces = FALSE)
   expect_equal(c("NomeProprio","surname"), names(nomes_retorno))
   expect_equal(nomes_proprios, nomes_retorno)
 })
