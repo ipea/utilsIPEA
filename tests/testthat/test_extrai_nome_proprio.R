912f37d0238325fdaf8a8156b2b2aee8a5154088
igornoberto <igor.noberto@outlook.com>
2017-10-24 09:53:36 -0200

    Corrige testes

--- tests/testthat/test_extrai_nome_proprio.R
+++ tests/testthat/test_extrai_nome_proprio.R
@@ -8,17 +8,17 @@ test_that("nomes proprios", {
 
 test_that("nomes proprios com sexo", {
   nomes <- c("Edileusamaria Ferreira", "Carlos das Neves", "Pedro dos Anjos", "Ana Santos", "Maria Eduarda Almeida")
-  nomes_proprios <- data.frame(NomeProprio = c("EDILEUSA MARIA","CARLOS","PEDRO","ANA","MARIA EDUARDA"), gender = c(1,2,2,1,1), stringsAsFactors = FALSE)
-  nomes_retorno <- extrai_NomeProprio(nomes,gender = TRUE)
-  expect_equal(c("NomeProprio","gender"), names(nomes_retorno))
+  nomes_proprios <- data.frame(NomeProprio = c("EDILEUSA MARIA","CARLOS","PEDRO","ANA","MARIA EDUARDA"), surname = c("FERREIRA", "DAS NEVES", "DOS ANJOS", "SANTOS", "ALMEIDA"), gender = c(1,2,2,1,1), stringsAsFactors = FALSE)
+  nomes_retorno <- extrai_NomeProprio(nomes,gender = TRUE, surname = TRUE)
+  expect_equal(c("NomeProprio","surname","gender"), names(nomes_retorno))
   expect_equal(nomes_proprios, nomes_retorno)
 })
 
 
 test_that("nomes proprios com sobrenome", {
   nomes <- c("Carlos das Neves", "Pedro dos Anjos", "Ana Santos", "Edileusa maria Ferreira", "Maria Eduarda Almeida")
-  nomes_proprios <- data.frame(NomeProprio = c("CARLOS","PEDRO","ANA","EDILEUSAMARIA","MARIAEDUARDA"), surname = c("DASNEVES", "DOSANJOS", "SANTOS", "FERREIRA", "ALMEIDA"), stringsAsFactors = FALSE)
+  #nomes_proprios <- data.frame(NomeProprio = c("CARLOS","PEDRO","ANA","EDILEUSAMARIA","MARIAEDUARDA"), stringsAsFactors = FALSE)
+  nomes_proprios <- c("CARLOS","PEDRO","ANA","EDILEUSAMARIA","MARIAEDUARDA")
   nomes_retorno <- extrai_NomeProprio(nomes,surname = FALSE, spaces = FALSE)
-  expect_equal(c("NomeProprio","surname"), names(nomes_retorno))
   expect_equal(nomes_proprios, nomes_retorno)
 })
