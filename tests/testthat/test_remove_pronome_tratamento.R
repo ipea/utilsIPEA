0e8dce2cf1ec9cc0da59d7b2907f7271c94a6a6a
igornoberto <igor.noberto@outlook.com>
2017-10-25 10:45:50 -0200

    melhora testes

--- tests/testthat/test_remove_pronome_tratamento.R
+++ tests/testthat/test_remove_pronome_tratamento.R
@@ -7,8 +7,20 @@ test_that("remove_pronome_tratamento", {
   expect_equal(nomes_sem_pronomes, base$nome_sem_pron)
 })
 
+
+test_that("remove_pronome_tratamento data.frame e NA", {
+  nomes <- c("Dr. Joao das Neves", "Exmo. Sr. Pedro dos Anjos", "Maria das Gracas", NA)
+  nomes_sem_pronomes <- c("JOAO DAS NEVES", "PEDRO DOS ANJOS", "MARIA DAS GRACAS", NA)
+  base <- data.frame(nome = nomes)
+  base <- remove_pronome_tratamento(base, "nome")
+  expect_equal(c("nome", "nome_sem_pron"), names(base))
+  expect_equal(nomes_sem_pronomes, base$nome_sem_pron)
+})
+
 test_that("remove_pronome_tratamento de um vetor de caracteres", {
-  nomes <- c("Dr. Joao das Neves", "Exmo. Sr. Pedro dos Anjos", "Maria das Gracas")
-  nomes_sem_pronomes <- c("JOAO DAS NEVES", "PEDRO DOS ANJOS", "MARIA DAS GRACAS")
+  nomes <- c("Dr. Joao das Neves", "Exmo. Sr. Pedro dos Anjos", "Maria das Gracas", NA)
+  nomes_sem_pronomes <- c("JOAO DAS NEVES", "PEDRO DOS ANJOS", "MARIA DAS GRACAS", NA)
   expect_equal(nomes_sem_pronomes, remove_pronome_tratamento(nomes))
 })
+
+
