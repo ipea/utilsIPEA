f417050411eafd99ce5a2c8a78c786b56572920a
Gustavo Rodrigues Coelho <B15599226@ipea.gov.br>
2017-12-12 17:37:51 -0200

    Unicode character for a
    Signed-off-by: Gustavo Rodrigues Coelho <B15599226@ipea.gov.br>

--- R/contador_letras.R
+++ R/contador_letras.R
@@ -18,15 +18,15 @@
 contar_letras <- function(base, columns, suffix = NULL){
   stopifnot(is.character(columns))
   ret <- lapply(columns, function(column){
-    re <- data.table(a=str_count(base[[column]], 'a|A|á|Á|â|Â|ã|Â|á|À'),
+    re <- data.table(a=str_count(base[[column]], 'a|A|\u00E1|\u00C1|\u00E2|\u00C2|\u00E3|\u00C3|\u00E0|\u00C0'),
                      b=str_count(base[[column]], 'b|B'),
-                     c=str_count(base[[column]], 'c|C|ç|Ç'),
+                     c=str_count(base[[column]], "c|C|\u00E7|\u00C7"),
                      d=str_count(base[[column]], 'd|D'),
-                     e=str_count(base[[column]], 'e|E|é|É|ê|Ê|è|È'),
+                     e=str_count(base[[column]], "e|E|\u00E9|\u00C9|ê|Ê|è|È"),
                      f=str_count(base[[column]], 'f|F'),
                      g=str_count(base[[column]], 'g|G'),
                      h=str_count(base[[column]], 'h|H'),
-                     i=str_count(base[[column]], 'i|I|í|Í|î|Î|ì|Ì'),
+                     i=str_count(base[[column]], 'i|I|\u00ED|\u00CD|\u00EE|\u00CE|\u00EC|\u00CC'),
                      j=str_count(base[[column]], 'j|J'),
                      k=str_count(base[[column]], 'k|K'),
                      l=str_count(base[[column]], 'l|L'),
